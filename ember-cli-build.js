f8bfa18bd11740282eaf7888c3d65f4e109dfab0
Santiago Ferreira <san650@gmail.com>
2016-11-06 11:57:17 -0300

    Use ember-web-app to generate the manifest.json file

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
     fingerprint: {
-      exclude: ['images/icons', 'data/images']
+      exclude: ['data/images']
     }
   });
 
