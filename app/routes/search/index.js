88f3b91bbfdd352232bcf52bbb2d83bbcca995e4
Federico Kauffman <federico@wyeworks.com>
2016-11-29 14:59:23 -0300

    Get recipes with ember-data

--- app/routes/search/index.js
+++ app/routes/search/index.js
@@ -6,10 +6,7 @@ export default Ember.Route.extend({
   ajax: inject.service(),
 
   model() {
-    return this
-      .get('ajax')
-      .request('/api/recipes')
-      .then((response) => response.recipes);
+    return this.store.findAll('recipe');
   },
 
   setupController(controller, model) {
